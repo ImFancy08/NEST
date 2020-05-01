6f24076a069e20c2c2ef491aafc8cb020e3eb94b
Minh Phan <kennguhoc@gmail.com>
2020-05-01 01:41:48 -0400

    EnemyMoving

--- Assets/EnemyMoving.cs
+++ Assets/EnemyMoving.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMoving : MonoBehaviour
 {
+    [SerializeField]
+    Transform EndPoint;
+    NavMeshAgent EnemyMeshAgent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        EnemyMeshAgent = this.GetComponent<NavMeshAgent>();
+        if(EndPoint == null)
+        {
+            Debug.Log("Attach the destination point to the enemy please");
+        }
+        else
+        {
+            SetDestinationPoint();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetDestinationPoint()
     {
-        
+        if(EndPoint != null)
+        {
+            Vector3 target = EndPoint.transform.position;
+            EnemyMeshAgent.SetDestination(target);
+        }
     }
 }
