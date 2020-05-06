fe94e6ee31d048f7f3c734149c92fc4f294f22bf
Minh Phan <kennguhoc@gmail.com>
2020-05-06 15:16:13 -0400

    Add Main Menu
    +Main Menu UI
    +Main Menu Script

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject LoadingScreen;
+    public void StartGame(int indexScene)
+    {
+        StartCoroutine(LoadASync(indexScene + 1));
+    }
+
+    IEnumerator LoadASync (int indexScene)
+    {
+        AsyncOperation oper = SceneManager.LoadSceneAsync(indexScene);
+        while(!oper.isDone)
+        {
+            float prog = Mathf.Clamp01(oper.progress / 0.9f);
+            Debug.Log(prog);
+            yield return null;
+        }
+    }
+}
