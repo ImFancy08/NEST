4b680cbffbc71c869be80f61f896cd63c3969593
Minh Phan <kennguhoc@gmail.com>
2020-05-07 08:43:21 -0400

    Loading Scene
    Add Loading Scene went start the game

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
 public class MenuScript : MonoBehaviour
 {
     public GameObject LoadingScreen;
+    public Slider slider;
+    public Text progText;
+
     public void StartGame(int indexScene)
     {
         StartCoroutine(LoadASync(indexScene + 1));
@@ -15,10 +18,12 @@ public class MenuScript : MonoBehaviour
     IEnumerator LoadASync (int indexScene)
     {
         AsyncOperation oper = SceneManager.LoadSceneAsync(indexScene);
+        LoadingScreen.SetActive(true);
         while(!oper.isDone)
         {
             float prog = Mathf.Clamp01(oper.progress / 0.9f);
-            Debug.Log(prog);
+            slider.value = prog;
+            progText.text = prog * 100 + "%";
             yield return null;
         }
     }
