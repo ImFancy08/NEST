829320f4cff98ece11f85b5e305c5fe44976d556
Minh Phan <kennguhoc@gmail.com>
2020-05-13 12:12:37 -0400

    New Updated

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
+    public static GameManager gm;
+    bool gameStart;
     [SerializeField] public int numberofEnemy;
     [SerializeField] private Enemy enemy;
     [SerializeField] private int indexScene;
-    private Boolean isValid = true;
+
+    private void Awake()
+    {
+        if (!gameStart)
+        {
+            gm = this;
+            SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
+            gameStart = true;
+        }
+    }
     //LoadingScript load;
     // Start is called before the first frame update
     void Start()
@@ -16,26 +27,27 @@ public class GameManager : MonoBehaviour
         indexScene = SceneManager.GetActiveScene().buildIndex; //Get the index of the current Scene
         Debug.Log(indexScene);
         Debug.Log(SceneManager.sceneCountInBuildSettings);
+        Debug.Log(indexScene);
     }
     // Update is called once per frame
-    void Update()
-    {
-        Win();
-    }
-    public void Win()
-    {
-        numberofEnemy = GameObject.FindGameObjectsWithTag("Red Ant").Length;
-        Debug.Log("Enemy:" + numberofEnemy);
-        if (numberofEnemy == 0)
-        {
-            if (indexScene +1 < SceneManager.sceneCountInBuildSettings)// 3 + 1 != 4 is false
-            {                                                           
-                SceneManager.LoadScene(indexScene + 1);
-            }
-            else                                      
-            {
-                SceneManager.LoadScene(0);
-            }
-        }
-    }
+    //void Update()
+    //{
+    //    Win();
+    //}
+    //public void Win()
+    //{
+    //    numberofEnemy = GameObject.FindGameObjectsWithTag("Red Ant").Length;
+    //    Debug.Log("Enemy:" + numberofEnemy);
+    //    if (numberofEnemy == 0)
+    //    {
+    //        if (indexScene +1 < SceneManager.sceneCountInBuildSettings)// 3 + 1 != 4 is false
+    //        {                                                           
+    //            SceneManager.LoadScene(indexScene + 1);
+    //        }
+    //        else                                      
+    //        {
+    //            SceneManager.LoadScene(0);
+    //        }
+    //    }
+    //}
 }
