6f7c3df2caaae5684941a34419f21c3248ec52b3
Minh Phan <kennguhoc@gmail.com>
2020-05-23 01:36:23 -0400

    Spawn BAnt
    + Click on the brick to Spawn BAnt

---
+++ Assets/Building.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public static Building instance;
+
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("More than one Building is in the scene");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject normalBAnt;
+
+    private void Start()
+    {
+        turretToBuild = normalBAnt;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurret()
+    {
+        return turretToBuild;
+    }
+}
--- Assets/Place.cs
+++ Assets/Place.cs
@@ -3,14 +3,30 @@
 public class Place : MonoBehaviour
 {
     public Color placeColor;
-    private Color StartColor;
+
+    private GameObject currentTurret;
 
     private Renderer rend;
+    private Color StartColor;
+    [SerializeField] private Vector3 posOffset;
 
     void Start()
     {
         rend = GetComponent<Renderer>();
         StartColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if(currentTurret!= null)
+        {
+            Debug.Log("Can't build there! - TODO: Display on Screen");
+            return;
+        }
+
+        //Build a turret
+        GameObject turretToBuild = Building.instance.GetTurret();
+        currentTurret = (GameObject) Instantiate(turretToBuild, transform.position + posOffset, transform.rotation);
     }
 
     private void OnMouseEnter()
