2e95602a263bfe29943e3369f71a8b5b38182e93
Minh Phan <kennguhoc@gmail.com>
2020-05-22 23:30:19 -0400

    Place Brick Detection

---
+++ Assets/Place.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Place : MonoBehaviour
+{
+    public Color placeColor;
+    private Color StartColor;
+
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        StartColor = rend.material.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = placeColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = StartColor;
+    }
+
+}
