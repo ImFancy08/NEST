3e2f7287e9ec9ef9298d34c25afa4970ad1c653a
Minh Phan <kennguhoc@gmail.com>
2020-05-25 03:13:00 -0400

    UI Shop Added(Raw)

---
+++ Assets/Shop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public void BuyNormalBAnt()
+    {
+        Debug.Log("Buy Normal Black Ant");
+    }
+    public void BuySlowBAnt()
+    {
+        Debug.Log("Buy Slow Black Ant");
+    }
+    public void BuyLaserBAnt()
+    {
+        Debug.Log("Buy Laser Black Ant");
+    }
+}
